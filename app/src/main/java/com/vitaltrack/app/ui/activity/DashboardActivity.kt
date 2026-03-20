6a35aa5ab805ce3a85f11949323d8eb8565aa4f9
PedroKeita <pedrolucas300805@gmail.com>
2026-03-20 18:41:28 -0300

    feature: Add dashboard screen and logic navigation implemented

---
+++ app/src/main/java/com/vitaltrack/app/ui/activity/DashboardActivity.kt
@@ -0,0 +1,16 @@
+package com.vitaltrack.app.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.vitaltrack.app.databinding.ActivityDashboardBinding
+
+class DashboardActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDashboardBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDashboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
